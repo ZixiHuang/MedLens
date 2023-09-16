a374557f1223dc9590b82d4d66ea400e947e51e3
chujianyu <yu_chn@outlook.com>
2023-09-16 17:01:00 -0400

    Update OCR
    Co-authored-by: JoannaCCJH <JoannaCCJH@users.noreply.github.com>

--- python/ocr.py
+++ python/ocr.py
@@ -1,6 +1,7 @@
 from google.cloud import vision
 from google.cloud import videointelligence
 
+import cv2
 import io
 import numpy as np
 
@@ -54,19 +55,55 @@ def video_detect_text(path, silence=True):
 
     return detected_texts
 
+def get_highest_score_image(frame, objects, width, height):
+    if objects is None or len(objects) == 0:
+        return None
+    objects.sort(key=lambda obj: obj.score, reverse=True)
+    top = objects[0]
+    bounding = top.bounding_poly
+    x1, y1 = int(bounding.normalized_vertices[0].x * width), int(bounding.normalized_vertices[0].y * height)
+    x3, y3 = int(bounding.normalized_vertices[2].x * width), int(bounding.normalized_vertices[2].y * height)
+
+    frame = cv2.flip(frame, 1)
+    cropped_image = frame[y1:y3, x1:x3]
+
+    is_success, im_buf_arr = cv2.imencode(".jpg", cropped_image)
+    if not is_success:
+        return None
+
+    return im_buf_arr.tobytes()
 
 
-def detect_text(path, silence=True):
+def get_ocr_result(client, frame, objects, width, height, silence = True):
+    concat_texts = ""
+    concat_texts_w_bbox = []
+    img_bytes = get_highest_score_image(frame, objects, width, height)
+    if img_bytes:
+        text, texts_w_bbox = detect_text(client, img_bytes = img_bytes, silence = silence)
+        if text:
+            concat_texts += text
+        if texts_w_bbox:
+            concat_texts_w_bbox.append(texts_w_bbox)
+    if not silence:
+        print(concat_texts)
+    return concat_texts, concat_texts_w_bbox
 
-    client = vision.ImageAnnotatorClient()
 
-    with open(path, "rb") as image_file:
-        content = image_file.read()
+def detect_text(client, path = None, img_bytes = None, silence=True):
+    if path is None and img_bytes is None:
+        return None, None
 
-    image = vision.Image(content=content)
+    
+
+    if img_bytes is None:
+        with open(path, "rb") as image_file:
+            img_bytes = image_file.read()
+    image = vision.Image(content=img_bytes)
     text_detection_params = vision.TextDetectionParams(enable_text_detection_confidence_score=True)
     image_context = vision.ImageContext(text_detection_params=text_detection_params)
     response = client.text_detection(image=image, image_context=image_context)
+    if len(response.text_annotations) == 0:
+        return None, None
     ordered_text = response.text_annotations[0].description
     texts_w_bbox = []
 
@@ -104,7 +141,7 @@ def detect_text(path, silence=True):
     return ordered_text, texts_w_bbox
 
 if __name__ == "__main__":
-    video_path = './data/coke.mov'
-    photo_path = './data/clindamycin.jpg'
-    video_detect_text(video_path, silence=False)
-    detect_text(photo_path, silence=False)
+    video_path = '../data/coke.mov'
+    photo_path = '/Users/chujian/Documents/School/FA23/Hophacks/project/Hophack_2023/data/clindamycin.jpg'
+    # video_detect_text(video_path, silence=False)
+    detect_text(path=photo_path, silence=False)
