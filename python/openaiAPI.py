e4f47070562eaa5c2a633b00c9cab9b35a46d7c0
黄子曦 <kevinhuang@huangzixidembp.acasmart.jh.edu>
2023-09-16 18:22:55 -0400

    encapsulate generating chatgpt response in a function

--- python/openaiAPI.py
+++ python/openaiAPI.py
@@ -2,20 +2,28 @@ import openai
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+sample_conversation = { 'user': '',
+                        'assistant': '' }
 
-# Get API key from environment variable
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def generate_response(patient_info: str, med_info: str):
+    # Load environment variables from .env file
+  load_dotenv()
 
-completion = openai.ChatCompletion.create(
-  model = "gpt-3.5-turbo",
-  temperature = 0.8,
-  max_tokens = 2048,
-  messages = [
-    {"role": "system", "content": "You are a medical consultant. You will be provided information of a medicine. Your job is to summarize important information related to the medicine."},
-    {"role": "user", "content": "Aspirin"}
-  ]
-)
+  # Get API key from environment variable
+  openai.api_key = os.getenv("OPENAI_API_KEY")
 
-print(completion.choices[0].message["content"])
+  completion = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    temperature = 0.8,
+    max_tokens = 2048,
+    messages = [
+      {"role": "system", "content": "You are a medical consultant. You will be provided information of a patient and a specific medicine. \
+      Your job is to summarize important information of the medicine and offer advice on how the patient should take the medicine based on his/hers information."},
+      {"role": "user", "content": f'The patient information is as follows. {patient_info} \n \
+       The medicine information is as follows. {med_info}' }
+    ]
+  )
+
+  return completion.choices[0].message["content"]
+
+# print(generate_response('no allergy', 'aspirin'))
